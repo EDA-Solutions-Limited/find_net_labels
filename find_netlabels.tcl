2d68bf93561b7ba881868fa9fd848c332bac9f08
raymondtliu <raymondliu@eda-solutions.com>
2023-03-02 10:09:22 +0000

    Typo corrections and formatting

---
+++ find_netlabels.tcl
@@ -0,0 +1,110 @@
+################################################################################
+# Name: find_netlabels.tcl                                                     #
+# Purpose: Uses a hotkey to find find_netlabels                                #
+#                                                                              #
+# Release: 03/Aug/2021                                                         #
+# Contact: Support@eda-solutions.com                                           #
+#                                                                              #
+# Please use this script at your own discretion and responsbility. Eventhough  #
+# This script was tested and passed the QA criteria to meet the intended       #
+# specifications and behaviors upon request, the user remains the primary      #
+# responsible for the sanity of the results produced by the script.            #
+# The user is always advised to check the imported design and make sure the    #
+# correct data is present.                                                     #
+#                                                                              #
+# For further support or questions, please e-mail support@eda-solutions.com    #
+#                                                                              #
+# Test platform version: S-Edit 2021.2 Update 0 Release build                  #
+################################################################################
+# --------------------------
+# Installation:- 
+# --------------------------
+# 1. adjust line 54 to the desired zoom level
+# 2. for one time use drag and drop the script into S-edit command window,
+# for persistent use copy the script into the 
+# '%APPDATA%\Roaming\Tanner EDA\scripts\startup.sedit' folder
+# --------------------------
+# Usage:-
+# --------------------------
+# 1. Select a netlabel
+# 2. Use the hotkey combination ‘Ctrl+Alt+F’ to Find instances
+# 3. if the command window indicate there is more instances keep pressing
+# ‘Ctrl+Alt+F’ to scroll through
+# 4. to search for a new label select the new label and press the hotkey combo
+# --------------------------
+# Notes:
+# --------------------------
+# . the script is a demo script to demonstrate how you can leverage tcl to
+# assign hotkeys to find functionality. this script could be enhanced to allow
+# a hotkey to scroll back through the list of selected net labes
+################################################################################
+#########################################################################
+#                                                                       #
+#   History:                                                            #
+#   Version 0.0 |- Created  script                                      #
+#           1.0 | 07/09/2022 - Script file first created                #
+#           1.1 | 07/09/2022 - Script completed                         #
+#########################################################################
+    
+    
+variable nName
+variable count
+variable nLabelC
+set nName ""
+setup selection set -zoompercent 30    
+proc find_labels {} {
+    #get the name of the select variable
+    global nName
+    global count
+    global nLabelC
+
+    #figure out if func been run before, run before but new port selected, or continue incrementing
+    if {[llength $nName] == 0} {
+        # never run before
+        set nName [property get -system -name Name]
+        #test is selected item a net label. 0 means no.
+        set nLabelC [find netlabel -scope view -name $nName -nocase -contains -count]
+        if {$nLabelC == 0} { puts "ERROR: No labels found, please select a valid label"; return 1}
+        #clear the count which highlights everything
+        find netlabel -scope view -name -nocase -contains -first
+        highlight -net $nName
+        #restart the scroll counter
+        set count 1
+    } elseif {[property get -system -name Name] != $nName} {
+        # run before but new label selected
+        set nName [property get -system -name Name]
+        #test is selected item a net label. 0 means no.
+        set nLabelC [find netlabel -scope view -name $nName -nocase -contains -count]
+        if {$nLabelC == 0} { puts "ERROR: No labels found, please select a valid label"; return 1}
+        #clear the count which highlights everything
+        find netlabel -scope view -name -nocase -contains -first
+        highlight -net $nName
+        #restart the scroll counter
+        set count 1
+    } elseif {$count == $nLabelC} {
+        #no more labels!
+        set nName ""
+        puts "/////////////////////////////////"
+        puts "SCRIPT WARNING No more labels!"
+        puts "/////////////////////////////////"
+    } else {
+        #continue with numbering
+        set a [expr $count + 1]
+        set count $a
+    }
+    #gaurd cluase to exit the function if the label count is zero. i.e no label selected
+    if {$nLabelC == 0} {puts "ERROR: No labels found, please select a valid label"; return 1}
+    find netlabel -scope view -name $nName -nocase -contains -next
+    #only print statment once when count is 1
+    if {$count == 1} {puts "SCRIPT FOUND: $nLabelC of $nName net labels"}
+    puts "SCRIPT  NOTE: $count/$nLabelC"
+}
+
+workspace menu -name {EDA_Scripts "Find_nodes"} -command find_labels
+
+workspace bindkeys -command {find_labels} -key "Ctrl+Alt+F"
+
+puts "/////////////////////////////////"
+puts "SCRIPT LOADED: find_netlabels"
+puts "SCRIPT ADDED SHORTCUT: find_labels (Ctrl+Alt+F)"
+puts "/////////////////////////////////"
---
+++ readme.md
@@ -0,0 +1,92 @@
+# find_net_labels
+
+<!-- PROJECT LOGO -->
+<p align="center">
+  <br />
+  <a href="https://github.com/EDA-Solutions-Limited/find_net_labels"><strong>Explore the project »</strong></a>
+  <br />
+  <a href="https://github.com/EDA-Solutions-Limited/find_net_labels/issues">Report Bug</a>
+  ·
+  <a href="https://github.com/EDA-Solutions-Limited/find_net_labels/issues">Request Feature</a>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+Adds the ability to select a net in S-edit and then cycles through net labels with the same key using a hotkey combination.
+
+### Built With
+
+* [Tcl](https://www.tcl.tk/about/language.html)
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Run as a Tcl script in S-edit
+1. Select a net
+2. Press the hotkey combo 'Ctrl+Alt+F'
+
+### Prerequisites
+
+S-edit schematic design.
+
+### Installation
+
+Download file to a read directory
+
+### Notes:
+You can also clone the repo:
+```sh
+  git clone https://github.com/EDA-Solutions-Limited/find_net_labels.git
+  ```
+
+<!-- ROADMAP -->
+## Roadmap
+
+1. Add the ability to go to a previous net label
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+<!-- LICENSE -->
+## License
+GNU General Public License v3.0
+
+<!-- CONTACT -->
+## Contact
+support@eda-solutions.com
